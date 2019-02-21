08e935f56fe1fb26e2ee9fbbe96bccf14db219b7
sp0xff <sp0xff@gmail.com>
2019-02-21 15:35:33 +0500

    убрал Job

--- app/src/main/java/com/pavel/rebrain/screen/splash/SplashActivity.kt
+++ app/src/main/java/com/pavel/rebrain/screen/splash/SplashActivity.kt
@@ -3,31 +3,24 @@ package com.pavel.rebrain.screen.splash
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.pavel.rebrain.R
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 
 class SplashActivity : AppCompatActivity(), CoroutineScope {
 
-    private lateinit var mJob: Job
     override val coroutineContext: CoroutineContext
-        get() = mJob + Dispatchers.Default
+        get() = Dispatchers.Default
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        mJob = Job()
-
         launch {
             delay(500)
             finish()
         }
-
     }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        mJob.cancel()
-    }
-
 }

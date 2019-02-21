4e5bb4922e3e13272bcbf158b2e88e9bc0640d6c
sp0xff <sp0xff@gmail.com>
2019-02-21 17:32:40 +0500

    fullscreen, если sdk < 19

--- app/src/main/java/com/pavel/rebrain/screen/splash/SplashActivity.kt
+++ app/src/main/java/com/pavel/rebrain/screen/splash/SplashActivity.kt
@@ -1,6 +1,8 @@
 package com.pavel.rebrain.screen.splash
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.pavel.rebrain.R
 import kotlinx.coroutines.CoroutineScope
@@ -9,6 +11,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 
+
 class SplashActivity : AppCompatActivity(), CoroutineScope {
 
     override val coroutineContext: CoroutineContext
@@ -16,6 +19,12 @@ class SplashActivity : AppCompatActivity(), CoroutineScope {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // если sdk < 19, то fullscreen. в остальных случаях прозрачный Status Bar заданный в настройках стиля
+        if (Build.VERSION.SDK_INT in 16 until 19) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
+
         setContentView(R.layout.activity_splash)
 
         launch {

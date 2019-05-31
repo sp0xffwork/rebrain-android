c487687fc6d4e2b695caf4336c7d5425025f2359
sp0xff <sp0xff@gmail.com>
2019-05-31 23:20:36 +0500

    используем attach/detach

--- app/src/main/java/com/pavel/rebrain/screen/main/MainActivity.kt
+++ app/src/main/java/com/pavel/rebrain/screen/main/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.pavel.rebrain.R
 import com.pavel.rebrain.screen.base.BaseActivity
 import com.pavel.rebrain.screen.base.BaseFragment
@@ -28,24 +29,42 @@ class MainActivity : BaseActivity("MainActivity"), OnFragmentInteractionListener
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val fragmentMain: BaseFragment
+        val fragmentFavorites: BaseFragment
+
         if (savedInstanceState == null) {
-            bottomBar.setCheckedButton(BottomBar.MainTabType.MAIN)
-            setFragment(MainTabFragment.newInstance())
+            fragmentMain = MainTabFragment.newInstance()
+            fragmentFavorites = FavoritesTabFragment.newInstance()
+            supportFragmentManager.beginTransaction().add(R.id.container, fragmentMain, fragmentMain.getFragmentTag())
+                .commit()
+            supportFragmentManager.beginTransaction()
+                .add(R.id.container, fragmentFavorites, fragmentFavorites.getFragmentTag()).commit()
+        } else {
+            fragmentMain = supportFragmentManager.findFragmentByTag("MainTabFragment") as BaseFragment
+            fragmentFavorites = supportFragmentManager.findFragmentByTag("FavoritesTabFragment") as BaseFragment
         }
 
         bottomBar.setOnTabClickListener(BottomBar.MainTabType.MAIN) {
-            setFragment(MainTabFragment.newInstance())
+            setFragment(fragmentMain)
         }
 
         bottomBar.setOnTabClickListener(BottomBar.MainTabType.FAVORITES) {
-            setFragment(FavoritesTabFragment.newInstance())
+            setFragment(fragmentFavorites)
         }
+
+        //todo сохранять какой фрагмент активен и восстанавливать при пересоздании activity
+        bottomBar.setCheckedButton(BottomBar.MainTabType.MAIN)
+        setFragment(fragmentMain)
     }
 
     private fun setFragment(fragment: BaseFragment) {
+        val fragmentMain = supportFragmentManager.findFragmentByTag("MainTabFragment")
+        val fragmentFavorites = supportFragmentManager.findFragmentByTag("FavoritesTabFragment")
+        fragmentMain?.let { supportFragmentManager.beginTransaction().detach(it).commit() }
+        fragmentFavorites?.let { supportFragmentManager.beginTransaction().detach(it).commit() }
         supportFragmentManager
             .beginTransaction()
-            .replace(R.id.container, fragment, fragment.getFragmentTag())
+            .attach(fragment)
             .commit()
     }
 

981d5ebd1139512ca53286641a3e669bc195d8d5
sp0xff <sp0xff@gmail.com>
2019-12-09 01:26:39 +0500

    dagger - ProductModeStorageModule

---
+++ app/src/main/java/com/pavel/rebrain/di/ProductModeStorageModule.kt
@@ -0,0 +1,23 @@
+package com.pavel.rebrain.di
+
+import com.pavel.rebrain.domain.TableMode
+import com.pavel.rebrain.repository.ProductModeStorage
+import com.pavel.rebrain.repository.Storage
+import com.pavel.rebrain.util.PreferenceHelper
+import dagger.Module
+import dagger.Provides
+
+/**
+ * будет создавать имплементацию ProductModeStorage
+ */
+
+@Module
+class ProductModeStorageModule {
+
+    @Provides
+    fun provideStorage(preferenceHelper: PreferenceHelper): Storage<TableMode> =
+        ProductModeStorage(preferenceHelper)
+
+}
+
+

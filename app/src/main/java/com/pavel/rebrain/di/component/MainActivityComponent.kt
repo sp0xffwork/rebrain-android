40bed9c3bad3cde4c5067ef60bf3030fcf392b5c
sp0xff <sp0xff@gmail.com>
2019-12-16 20:55:31 +0500

    добавлены компоненты для всех экранов

---
+++ app/src/main/java/com/pavel/rebrain/di/component/MainActivityComponent.kt
@@ -0,0 +1,12 @@
+package com.pavel.rebrain.di.component
+
+import dagger.Component
+import javax.inject.Singleton
+
+/**
+ * компонент для MainActivity
+ */
+@Component()
+@Singleton
+interface MainActivityComponent {
+}
